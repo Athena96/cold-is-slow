8c2930cb9c5e2c6ed20f4d87f63b3d4df114a6f3
Jared Franzone <jffranzo@amazon.com>
2023-04-28 11:14:31 -0700

    fix: serialization error with props

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,18 @@ const MAX_DISPLAY = 5
 
 export async function getServerSideProps() {
   const posts = await getAllFilesFrontMatter('blog')
-  const date = new Date();
-  return { props: { posts, currentDate: date } }
+  const date = new Date()
+  return { props: { posts, currentDateString: date.toString() } }
 }
 
-export default function Home({ posts, currentDate }) {
+export default function Home({ posts, currentDateString }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest Test {currentDate.toString()}
+            Latest Test {currentDateString}
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
